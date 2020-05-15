3bf4c5b68724eb9b5edb5dde5d1397921d78308e
0xboz <0xboz@hacari.com>
2020-05-14 20:33:10 -0400

    Added uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,39 @@
+PYENV_ROOT="${HOME}/.pyenv"
+
+rm -fr $HOME/.pyenv
+
+shell="$1"
+if [ -z "$shell" ]; then
+shell="$(ps c -p "$PPID" -o 'ucomm=' 2>/dev/null || true)"
+shell="${shell##-}"
+shell="${shell%% *}"
+shell="$(basename "${shell:-$SHELL}")"
+fi
+
+case "$shell" in
+bash )
+    profile="$HOME/.bashrc"
+    ;;
+zsh )
+    profile="$HOME/.zshrc"
+    ;;
+ksh )
+    profile="$HOME/.profile"
+    ;;
+fish )
+    profile="$HOME/.config/fish/config.fish"
+    ;;
+esac
+
+case "$shell" in
+fish )
+sed -i "/set -x PATH \"${PYENV_ROOT}/bin\" \$PATH/d" ${profile}
+sed -i "/status --is-interactive; and . (pyenv init -|psub)/d" ${profile}
+sed -i "/status --is-interactive; and . (pyenv virtualenv-init -|psub)/d" ${profile}
+;;
+* )
+sed -i "/export PATH=\"${PYENV_ROOT}/bin:\$PATH\"/d" ${profile}
+sed -i "/eval \"\$(pyenv init -)\"/d" ${profile}
+sed -i "/eval \"\$(pyenv virtualenv-init -)\"/d" ${profile}
+;;
+esac
